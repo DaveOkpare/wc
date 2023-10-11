fe04c30541847bb4ad3cb1c01a2c54fa5e714d43
DaveOkpare <okparedave@gmail.com>
2023-10-11 18:59:35 +0100

    Refactor conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
-import os
 import pytest
+
+CONTENT = "This is a test.\nThis is another test.\n"
 
 
 @pytest.fixture
 def temp_test_file(tmpdir):
     file_path = tmpdir.join("temp_test_file.txt")
-    with open(file_path, "w") as temp_file:
-        temp_file.write("This is a test.\nThis is another test.\n")
+    file_path.write_text(CONTENT, encoding="utf-8")
     yield file_path
-    os.remove(file_path)
